06b68f12c913aa8a551e8206f93b61e3dc20144e
powerboat9 <powerboat9@users.noreply.github.com>
2016-03-20 14:49:10 -0400

    Made more verbose

--- install.lua
+++ install.lua
@@ -43,6 +43,6 @@ shell.dir = function()
 end
 
 _G.fs.combine = function(s1, s2)
-    print("combining")
+    print("combining " .. s1 .. " " .. s2)
     return old.combine(old.dir(), old.combine(s1, s2))
 end
